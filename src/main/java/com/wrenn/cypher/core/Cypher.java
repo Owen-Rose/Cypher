0477d3aca4c33045f4e66e4dc0065b6cbcfd2c0f
Owen-Rose <owen.rose@edu.sait.ca>
2024-02-10 00:21:17 -0700

    Added singleton pattern to Cypher class

--- src/main/java/com/wrenn/cypher/core/Cypher.java
+++ src/main/java/com/wrenn/cypher/core/Cypher.java
@@ -1,4 +1,16 @@
 package com.wrenn.cypher.core;
 
 public class Cypher {
+    private static Cypher instance;
+
+    private Cypher() {
+
+    }
+
+    public static Cypher getInstance() {
+        if (instance == null) {
+            instance = new Cypher();
+        }
+        return instance;
+    }
 }
